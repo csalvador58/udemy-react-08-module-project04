64d637a85d417bf9e13f374c4f4ae413c620739c
Chris Salvador <csalvador58@gmail.com>
2023-01-18 22:59:06 -0800

    Refactored loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,11 +5,18 @@ import EventsList from '../components/EventsList';
 function EventsPage() {
   const events = useLoaderData();
 
-  return (
-    <>
-      <EventsList events={events} />
-    </>
-  );
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+                // ...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+}
